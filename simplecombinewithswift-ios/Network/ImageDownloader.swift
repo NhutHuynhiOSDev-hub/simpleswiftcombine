9e9f1ccc8b86ecc00139eeac6af77d451c2d7f37
Huỳnh Quang Nhựt <nhut.hq@seednet.edu.vn>
2021-02-28 12:10:44 +0700

    update to Combine

--- simplecombinewithswift-ios/Network/ImageDownloader.swift
+++ simplecombinewithswift-ios/Network/ImageDownloader.swift
@@ -5,24 +5,52 @@
 //  Created by Nhut Huynh on 28/02/2021.
 //
 
-import Foundation
 import UIKit
+import Combine
+import Foundation
+
 
 enum ImageDownloader {
     
-    static func download(url: String, completion: @escaping (UIImage?) -> Void) {
+    static func download(url: String) -> AnyPublisher<UIImage, GameError> {
+    
+        guard let url = URL(string: url) else {
         
-        let url = URL(string: url)!
-        URLSession.shared.dataTask(with: url) { data, response, error in
+            return Fail(error: GameError.invalidURL)
+                .eraseToAnyPublisher()
+            
+        }
         
-            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                  let data = data, error == nil,
-                  let image = UIImage(data: data)
-            else {
-                completion(nil)
-                return
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { response -> Data in
+          
+                guard let httpURLResponse = response.response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else { throw GameError.statusCode }
+                return response.data
             }
-            completion(image)
-        }.resume()
+            .tryMap { data in
+          
+                guard let image = UIImage(data: data) else { throw GameError.invalidImage }
+                
+                return image
+            }
+            .mapError { GameError.map($0) }
+            .eraseToAnyPublisher()
     }
+
+    
+//    static func download(url: String, completion: @escaping (UIImage?) -> Void) {
+//
+//        let url = URL(string: url)!
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//
+//            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+//                  let data = data, error == nil,
+//                  let image = UIImage(data: data)
+//            else {
+//                completion(nil)
+//                return
+//            }
+//            completion(image)
+//        }.resume()
+//    }
 }
